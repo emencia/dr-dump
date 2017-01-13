97bc802c4adf42dc0b1d763df9c0982d97f34fb5
Grégoire ROCHER <gr@enix.org>
2017-01-13 15:52:07 +0100

    Py2 compat

--- drdump/management/commands/dr_dump.py
+++ drdump/management/commands/dr_dump.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        super().add_arguments(parser)
+        super(Command, self).add_arguments(parser)
         parser.add_argument(
             '-m',
             '--map',
